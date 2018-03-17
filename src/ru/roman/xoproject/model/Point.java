a8c746e3b47b2ee3778be3243c776712348d1d04
Roman <romavr99@gmail.com>
2018-03-17 18:51:00 +0300

    class Point changed

--- src/ru/roman/xoproject/model/Point.java
+++ src/ru/roman/xoproject/model/Point.java
@@ -1,5 +1,21 @@
 package ru.roman.xoproject.model;
 
 public class Point {
+	
+	final private int x;	
+
+	final private int y;	
+	
+	public Point(final int x, final int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}	
 
 }
