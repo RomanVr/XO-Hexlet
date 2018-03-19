e78e0ad63c714b2924e197f0a9199c9e34285a05
Roman <romavr99@gmail.com>
2018-03-19 21:51:41 +0300

    FieldTest - added, no implements

---
+++ src.main.test/ru/roman/xoproject/model/FieldTest.java
@@ -0,0 +1,30 @@
+package ru.roman.xoproject.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FieldTest {
+
+	@Before
+	public void setUp() throws Exception {
+		Field<FigureXO> field = new Field<>(3);		
+	}
+
+	@Test
+	public void testGetFigure() {
+		
+	}
+
+	@Test
+	public void testSetFigure() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSize() {
+		fail("Not yet implemented");
+	}
+
+}
