01db5ab60bf74d27598d113fe9c62c5d88f84ddc
Roman <romavr99@gmail.com>
2018-03-19 20:54:17 +0300

    Change class model Field, add exception

--- src/ru/roman/xoproject/model/Field.java
+++ src/ru/roman/xoproject/model/Field.java
@@ -1,6 +1,10 @@
 package ru.roman.xoproject.model;
 
+import ru.roman.xoproject.model.exceptions.InvalidPointException;
+
 public class Field<T> {
+	
+	private static final int MIN_COORDINATE = 0;
 	
 	private final T[][] figures;
 	
@@ -12,15 +16,31 @@ public class Field<T> {
 		this.figures = (T[][])new Object[size][size];
 	}
 
-	public T getFigure(final Point point) {
+	public T getFigure(final Point point) throws InvalidPointException {
+		if (!checkPoint(point)) {
+		             throw new InvalidPointException();
+		}
 		return figures[point.getX()][point.getY()];
 	}
 	
-	public void setFigure(final Point point, final T figure) {
+	
+
+	public void setFigure(final Point point, final T figure) throws InvalidPointException {
 		figures[point.getX()][point.getY()] = figure;
 	}
 
 	public int getSize() {
 		return size;
+	}
+	
+	private boolean checkPoint(Point point) {
+		return checkCoordinate(point.getX(), figures.length) 
+	        && checkCoordinate(point.getY(), figures[point.getX()].length);
+	}
+	
+	private boolean checkCoordinate (final int coordinate, final int maxCoordinate) {
+		return coordinate >= MIN_COORDINATE 
+				&& 
+				coordinate < maxCoordinate;
 	}
 }
